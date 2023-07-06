987748c5d5c2ab2bfc067f48a7c51c9878e42ccb
Marvin Gill <marvg@Marvins-MBP.lan>
2023-07-06 16:43:15 -0400

    finished first tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import React from 'react';
+import './index.css'
+import { useState } from 'react'
 // Write your Color component here
-
+const Color = ({color}) => {
+  return <div className={color}></div>
+  
+}
 const App = () => {
   return (
+    
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
         <div className="red">red</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="blue" />
+        <Color color="orange" />
+        <Color color="green" />
+        </div>
     </div>
   );
 };
